c3212f30c0482b35b5b0c525014dbbed1fcd145a
baagena <baagena@gmail.com>
2024-07-29 17:15:38 +0200

    Add necessary components and use styled components to style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,43 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  /* background-color: #3c2525;
+  height: 100vh; */
+  padding: 20px;
+`;
+
 function App() {
-  const x = 12;
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check in</Button>
+              <Button variations="secondary" sizes="small">
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <div>
+              <Input type="text" placeholder="Number of Guest" />
+              <Input type="text" placeholder="Number of Guest" />
+            </div>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
