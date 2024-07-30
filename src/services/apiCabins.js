449f6cde0b4d289eeaa570b8c038e1a0c2927dc5
baagena <baagena@gmail.com>
2024-07-30 12:45:36 +0200

    Mutatio: delete a cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -8,8 +8,23 @@ export async function  getCabins() {
 if(error)
 {
     console.error(error);
-    throw new Error("No cabins found")
+    throw new Error("Cabin could not be loaded")
 }
 
+     return data;
+}
+
+export async function deleteCabin(id) {
+     
+const {data, error } = await supabase
+.from('cabins')
+.delete()
+.eq('id', id)
+if(error)
+     {
+     console.error(error);
+     throw new Error("Cabin could not be loaded")
+     }
+     
      return data;
 }
