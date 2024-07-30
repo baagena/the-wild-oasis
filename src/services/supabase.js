fa4b681dc20ec3c97d2877f54096192292ca90bd
baagena <baagena@gmail.com>
2024-07-30 10:09:39 +0200

    Create apiCabins service to link backend and front end

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://jnyumtavqjuqlgpqushg.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpueXVtdGF2cWp1cWxncHF1c2hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzMjE0MjcsImV4cCI6MjAzNzg5NzQyN30.dTAUd9MYbzVcXCOppbTWkJC16YYsnGB4jdKxw-uu5Qo"
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://jnyumtavqjuqlgpqushg.supabase.co';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpueXVtdGF2cWp1cWxncHF1c2hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzMjE0MjcsImV4cCI6MjAzNzg5NzQyN30.dTAUd9MYbzVcXCOppbTWkJC16YYsnGB4jdKxw-uu5Qo";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
+export default supabase;
