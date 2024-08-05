a3f858ce8266ea1cc2b5c28e0fcb392c6c120160
baagena <baagena@gmail.com>
2024-08-05 15:10:40 +0200

    Api-side filtering: Filter booking

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -2,9 +2,13 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 
-export async function getBookings() {
-  const {data, error} = await supabase.from("bookings").select("id ,created_at, startDate, endDate, numNights, numGuests, totalPrice, status, cabins(name), guests(fullName,email)");
+export async function getBookings({filter,sort}) {
 
+  let query =  supabase.from("bookings").select("id ,created_at, startDate, endDate, numNights, numGuests, totalPrice, status, cabins(name), guests(fullName,email)")
+
+  if(filter !== null) query = query[filter.method || "eq"](filter.field, filter.value)
+
+  const {data, error} = await query;
   if(error) {
     console.error(error)
     throw new Error("Bookings could not be loaded")
