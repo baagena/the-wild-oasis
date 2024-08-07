d05b977c3ea007765cc55b0c8060e495a3fd0af0
baagena <baagena@gmail.com>
2024-08-07 15:49:49 +0200

    Build the app header

---
+++ src/ui/HeaderMenus.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+
+const StyledHeaderMenus = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+function HeaderMenus() {
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderMenus>
+      <ButtonIcon onClick={() => navigate("/account")}>
+        <HiOutlineUser />
+      </ButtonIcon>
+      <Logout />
+    </StyledHeaderMenus>
+  );
+}
+
+export default HeaderMenus;
